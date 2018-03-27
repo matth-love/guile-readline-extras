b3af5d7558fcd6f9a2853645f16eb3e11bf89a23
Matthew Love <matthew.love@colorado.edu>
2018-03-26 18:09:04 -0700

    cleanup

--- src/readline_extras.c
+++ src/readline_extras.c
@@ -14,13 +14,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this file.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
  * --------------------------------------------------------------*/
 
-#include <stdio.h> 
 #include <libguile.h>
-
 #include <readline/readline.h>
 #include <readline/history.h>
 
@@ -30,9 +26,7 @@ SCM_DEFINE (scm_rl_parse_and_bind, "%rl-parse-and-bind", 1, 0, 0,
 {
   char *line;
   int status;
-
   line = scm_to_locale_string( scm_config_line );
-
   status = rl_parse_and_bind (line);
 
   if (status == 0) {
@@ -40,25 +34,13 @@ SCM_DEFINE (scm_rl_parse_and_bind, "%rl-parse-and-bind", 1, 0, 0,
   } else {
     return ( SCM_BOOL_F );
   }
-    
 }
-
-/* SCM */
-/* scm_readline_function (scm func) */
-/* { */
-
-/* } */
 
 void
 scm_init_readline_extras ()
 {
-  
-  //scm_c_define_gsubr ("%rl-parse-and-bind", 1, 0, 0, scm_rl_parse_and_bind);  
-
 #ifndef SCM_MAGIC_SNARFER
 #include "readline_extras.x"
 #endif
-
   scm_add_feature ("readline-extras");
-  
 }
